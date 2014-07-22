b96b8bd96a0c1fb3dde6b79e42c2718a022503bd
Jeffrey <jeffrey@wix.com>
2014-07-22 10:48:54 -0700

    Set up angular aspects of desktop mode - made title, description dependent on settings values, calendar only loads when view is in Month mode in settings

---
+++ app/client/scripts/controllers/desktop.js
@@ -0,0 +1,82 @@
+'use strict';
+
+angular.module('fbCal')
+  .controller('DesktopCtrl', function ($scope, $wix, api, $http, init, $log) {
+    $scope.settings = api.defaults;
+    
+
+
+    $log.log('hello world!!!!');
+
+    /* PUT THIS IN SETTINGS CALLBACK WHEN WRITTEN */
+    if ($scope.settings.view === "Month") {
+      var calendar = $("#calendar").calendar(
+        {
+           tmpl_path: "client/bower_components/bootstrap-calendar/tmpls/",
+           events_source: 
+           [
+              {
+                "id": 293,
+                "title": "Concert",
+                "url": "http://example.com",
+                "start": 1405811799000, // Milliseconds
+                "end": 1405943911000, // Milliseconds
+                "color": "#00FF00"
+              },
+              {
+                "id": 293,
+                "title": "Concert",
+                "url": "http://example.com",
+                "start": 1405811799000, // Milliseconds
+                "end": 1405943911000, // Milliseconds
+                "color": "#00FF00"
+              },
+              {
+                "id": 293,
+                "title": "Concert",
+                "url": "http://example.com",
+                "start": 1405811799000, // Milliseconds
+                "end": 1405943911000, // Milliseconds
+                "color": "#00FF00"
+              }
+            ],
+            onAfterEventsLoad: function(events) {
+              if(!events) {
+                return;
+              }
+              var list = $('#eventlist');
+              list.html('');
+
+              $.each(events, function(key, val) {
+                $(document.createElement('li'))
+                  .html('<a href="' + val.url + '">' + val.title + '</a>')
+                  .appendTo(list);
+              });
+            },
+            onAfterViewLoad: function(view) {
+              $('#current-view').text(this.getTitle());
+              $('.btn-group button').removeClass('active');
+              $('button[data-calendar-view="' + view + '"]').addClass('active');
+            }
+          });
+      $('.btn-group button[data-calendar-nav]').each(function() {
+        var $this = $(this);
+        $this.click(function() {
+          calendar.navigate($this.data('calendar-nav'));
+        });
+      });
+    }
+
+
+
+
+
+    /** 
+     * When the site owner updates the settings, this added event listener
+     * allows the widget to implement these changes immediately.
+     */
+    $wix.addEventListener($wix.Events.SETTINGS_UPDATED, function(message) {
+      $scope.settings = message;
+      $scope.$apply();
+    });
+});
