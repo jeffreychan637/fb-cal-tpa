a1f3b45a5d37439923f24770ee629e7116eea15a
Jeffrey <jeffrey@wix.com>
2014-07-25 16:21:01 -0700

    Wrote frontend code to make HTTP request to server to delete user info due to logout

--- app/client/scripts/services/server.js
+++ app/client/scripts/services/server.js
@@ -9,6 +9,7 @@ angular.module('fbCal').factory('server', function ($log, $http, $wix, api, $win
   var getSettingsSettingsURL = '/GetSettingsSettings/' + compId;
   var saveSettingsURL = '/SaveSettings/' + compId;
   var saveAccessTokenURL = '/SaveAccessToken/' + compId;
+  var logoutURL = '/Logout/' + compId;
 
   var defaultSettingsWidget = {settings : api.defaults, eventIds : [],
                                fb_event_data : {}, active : true};
@@ -45,25 +46,25 @@ angular.module('fbCal').factory('server', function ($log, $http, $wix, api, $win
    * is first loaded. On errors, the default settings are loaded.
    */
   var getUserInfo = function(from) {
-  $http({
-         method: 'GET',
-         url: getURL('get', from),
-         headers: {'X-Wix-Instance' : instance},
-         timeout: 10000
-        }).success(function (status, data) {
-          console.log(status, data);
-          if (status === 200) {
-            console.log(data);
-            return jQuery.parseJSON(data);
-          } else {
-            console.log('The server is returning an incorrect status.');
+    $http({
+           method: 'GET',
+           url: getURL('get', from),
+           headers: {'X-Wix-Instance' : instance},
+           timeout: 10000
+          }).success(function (status, data) {
+            console.log(status, data);
+            if (status === 200) {
+              console.log(data);
+              return jQuery.parseJSON(data);
+            } else {
+              console.log('The server is returning an incorrect status.');
+              return getDefault(from);
+              //i don't really know what the fb_event_data looks like
+            }
+          }).error(function (status, message) {
+            $log.warn(status, message);
             return getDefault(from);
-            //i don't really know what the fb_event_data looks like
-          }
-        }).error(function (status, message) {
-          $log.warn(status, message);
-          return getDefault(from);
-        });
+          });
   };
 
   var saveData = function(data, dataType) {
@@ -88,5 +89,30 @@ angular.module('fbCal').factory('server', function ($log, $http, $wix, api, $win
             console.log(message);
             return false;
           });
-    };
+  };
+
+  var logout = function() {
+    $http({
+            method: 'PUT',
+            url: logoutURL,
+            headers: {'X-Wix-Instance' : instance, 'URL' : $window.location.hostname},
+            timeout: 10000,
+            data: {}
+          }).success(function (status, message) {
+            console.log(status, message);
+            if (status === 200) {
+              console.log('Logged out successfully.');
+              return true;
+            } else {
+              console.log('The server is returning an incorrect status.');
+              return false;
+            }
+          }).error(function (status, message) {
+            console.log('Failed to logout.');
+            console.log(status);
+            console.log(message);
+            return false;
+          });
+  };
+
 });
