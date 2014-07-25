3bba74d8e6e4a015687adbc74ab867082d3d390c
Jeffrey <jeffrey@wix.com>
2014-07-25 12:24:24 -0700

    Finished post function for client to save data to server

--- app/client/scripts/services/server.js
+++ app/client/scripts/services/server.js
@@ -1,7 +1,7 @@
 'use strict';
 /*global $:false, FB:false, console:false, jQuery:false */
 
-angular.module('fbCal').factory('server', function ($log, $http, $wix, api) {
+angular.module('fbCal').factory('server', function ($log, $http, $wix, api, $window) {
   var compId = $wix.Utils.getCompId();
   var instance = api.getInstance();
   
@@ -27,8 +27,9 @@ angular.module('fbCal').factory('server', function ($log, $http, $wix, api) {
         return saveSettingsURL;
       } else {
         return saveAccessTokenURL;
+      }
     }
-  }
+  };
 
   var getDefault = function(from) {
     if (from === 'widget') {
@@ -43,95 +44,49 @@ angular.module('fbCal').factory('server', function ($log, $http, $wix, api) {
    * latest user settings. It's called whenever the widget or settings panal
    * is first loaded. On errors, the default settings are loaded.
    */
-  var getUserInfo = function(requestType, from) {
-  $http({method: 'GET',
-         url: getURL(requestType, from),
+  var getUserInfo = function(from) {
+  $http({
+         method: 'GET',
+         url: getURL('get', from),
          headers: {'X-Wix-Instance' : instance},
          timeout: 10000
-      }).success(function (data, status) {
-        console.log(data, status);
-        if (status === 200) {
-          console.log(data);
-          return jQuery.parseJSON(data);
-        } else {
-          console.log('The server is returning an incorrect status.');
+        }).success(function (status, data) {
+          console.log(status, data);
+          if (status === 200) {
+            console.log(data);
+            return jQuery.parseJSON(data);
+          } else {
+            console.log('The server is returning an incorrect status.');
+            return getDefault(from);
+            //i don't really know what the fb_event_data looks like
+          }
+        }).error(function (status, message) {
+          $log.warn(status, message);
           return getDefault(from);
-          //i don't really know what the fb_event_data looks like
-        }
-      }).error(function (status, message) {
-        $log.warn(status, message);
-        return getDefault(from);
-      });
+        });
   };
 
-var obtainSettings = function () {
-      $http.get('/api/settings/' + $scope.compId + '?userProfile=true', {
-            headers: {
-              'Content-type': 'application/json', 
-              'X-Wix-Instance': instance
-            }
-      }).success(function(data, status, headers, config) {
+  var saveData = function(data, dataType) {
+    $http({
+            method: 'PUT',
+            url: getURL('post', dataType),
+            headers: {'X-Wix-Instance' : instance, 'URL' : $window.location.hostname},
+            timeout: 10000,
+            data: data
+          }).success(function(status, data) {
+            console.log(status, data);
             if (status === 200) {
-              if (data.widgetSettings.hasOwnProperty("settings") && data.widgetSettings.settings != null) { //checks to see if there are saved settings
-              // if (Object.keys(data.widgetSettings.settings)) { 
-                console.log('there are saved settings');
-                $scope.settings = data.widgetSettings.settings; //works (this is if everything goes as planned and settings are gotten from the server)
-                $wix.UI.initialize($scope.settings);
-                $wix.Settings.triggerSettingsUpdatedEvent($scope.settings, 
-                  $wix.Utils.getOrigCompId());
-              } else {
-                console.log('there are no saved settings');
-                $scope.settings = api.defaults; // if user does not have any saved settings
-                $wix.UI.initialize($scope.settings);
-                $wix.Settings.triggerSettingsUpdatedEvent($scope.settings, 
-                  $wix.Utils.getOrigCompId());
-              }
+              console.log(dataType + ' saved successfully.');
+              return true;
             } else {
-              console.log("status != 200");
-              $scope.settings = api.defaults;
-              console.log('Initializing Wix UI Settings Panel');
-              $wix.UI.initialize($scope.settings);
-              $wix.Settings.triggerSettingsUpdatedEvent($scope.settings, 
-                $wix.Utils.getOrigCompId());
+              console.log('The server is returning an incorrect status.');
+              return false;
             }
-      }).error(function(data, status, headers, config) {
-          console.log("There was an error obtaining your saved settings from the database.");
-          $scope.settings = api.defaults;
-          $wix.UI.initialize($scope.settings);
-          $wix.Settings.triggerSettingsUpdatedEvent($scope.settings, 
-            $wix.Utils.getOrigCompId());
-          console.log('provider error: ' + $scope.provider);
-          // alert("There was an error obtaining your account settings.");
-      });
-    }
-
-
-  var putSettings = function () {
-      // Validates email. If invalid, uses last known valid email.
-      // var emailToSave = $scope.userReceiveEmail;
-      // console.log($scope.userReceiveEmail);
-      // console.log(emailRegex.test($scope.userReceiveEmail) === false);
-      // if (emailRegex.test($scope.userReceiveEmail) === false) {
-      //   emailToSave = previousValidEmail;
-      // }
-      var combineSettings = {widgetSettings: {settings: $scope.settings}};
-      var settingsJson = JSON.stringify(combineSettings);
-      var compId = $wix.Utils.getOrigCompId();
-      $http.put('/api/settings/' + compId + '?instance=' + instance, 
-         settingsJson,  { headers: {
-                      'X-Wix-Instance': instance,
-                      'Content-Type': 'application/json'
-                      }
-                    })
-          .success(function (data, status, headers, config) {
-          })
-          .error(function (data, status, headers, config) {
-            console.log("There was an error saving settings.");
-          })
-          .then(function (response) {
-            console.log("settings saved: " + response.data);
+          }).error(function(status, message) {
+            console.log(dataType + ' failed to save.');
+            console.log(status);
+            console.log(message);
+            return false;
           });
-        // console.log(emailToSave);
-      }
-
+    };
 });
