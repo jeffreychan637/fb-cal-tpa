99ca2207ee997505aba9a072e9d4263bec12cf44
Jeffrey <jeffrey@wix.com>
2014-07-25 11:45:36 -0700

    Wrote function for getting settings for widget and settings panel

--- app/client/scripts/services/server.js
+++ app/client/scripts/services/server.js
@@ -2,34 +2,65 @@
 /*global $:false, FB:false, console:false, jQuery:false */
 
 angular.module('fbCal').factory('server', function ($log, $http, $wix, api) {
-  var getSettingsWidgetURL = '/GetSettingsWidget/' + $wix.Utils.getCompId();
-  var getSettingsSettingsURL = '//';
-
-
+  var compId = $wix.Utils.getCompId();
   var instance = api.getInstance();
+  
+  var getSettingsWidgetURL = '/GetSettingsWidget/' + compId;
+  var getSettingsSettingsURL = '/GetSettingsSettings/' + compId;
+  var saveSettingsURL = '/SaveSettings/' + compId;
+  var saveAccessTokenURL = '/SaveAccessToken/' + compId;
+
+  var defaultSettingsWidget = {settings : api.defaults, eventIds : [],
+                               fb_event_data : {}, active : true};
+  var defaultSettingsSettings = {settings : api.defaults, eventIds : [],
+                                 active : true};
+
+  var getURL = function(requestType, from) {
+    if (requestType === 'get') {
+      if (from === 'widget') {
+        return getSettingsWidgetURL;
+      } else {
+        return getSettingsSettingsURL;
+      }
+    } else {
+      if (from === 'settings') {
+        return saveSettingsURL;
+      } else {
+        return saveAccessTokenURL;
+    }
+  }
+
+  var getDefault = function(from) {
+    if (from === 'widget') {
+      return defaultSettingsWidget;
+    } else {
+      return defaultSettingsSettings;
+    }
+  };
+
   /**
    * This function makes a call to the backend database to get the
-   * latest user settings. It's called whenever the widget is first loaded.
-   * On errors, the default settings are loaded.
+   * latest user settings. It's called whenever the widget or settings panal
+   * is first loaded. On errors, the default settings are loaded.
    */
-  var getUserInfo = function() {
+  var getUserInfo = function(requestType, from) {
   $http({method: 'GET',
-         url: getSettingsWidgetURL,
+         url: getURL(requestType, from),
          headers: {'X-Wix-Instance' : instance},
          timeout: 10000
       }).success(function (data, status) {
+        console.log(data, status);
         if (status === 200) {
           console.log(data);
           return jQuery.parseJSON(data);
         } else {
           console.log('The server is returning an incorrect status.');
-          return {settings : api.defaults, eventIds : [],
-                  fb_event_data : {}, active : true};
+          return getDefault(from);
           //i don't really know what the fb_event_data looks like
         }
-      }).error(function () {
-        return {settings : api.defaults, eventIds : [],
-                fb_event_data : {}, active : true};
+      }).error(function (status, message) {
+        $log.warn(status, message);
+        return getDefault(from);
       });
   };
 
@@ -63,20 +94,6 @@ var obtainSettings = function () {
               $wix.Settings.triggerSettingsUpdatedEvent($scope.settings, 
                 $wix.Utils.getOrigCompId());
             }
-        $scope.provider = data.widgetSettings.provider;
-        if ($scope.provider) {
-          $scope.userName = data.widgetSettings.userProfile.displayName;
-          $scope.userAccount = data.widgetSettings.userProfile.emails[0].value;
-          $scope.userReceiveEmail = $scope.userAccount;
-        } else {
-          $scope.userReceiveEmail = null;
-        }
-        previousValidEmail = $scope.userReceiveEmail;
-        console.log('provider success: ' + $scope.provider + ' ' + $scope.userReceiveEmail);
-        // console.log(data.widgetSettings);
-        // console.log(data.widgetSettings.settings);
-        // console.log(data.widgetSettings.userProfile);
-        // console.log(JSON.stringify(data.widgetSettings.userProfile.emails, null, 4));
       }).error(function(data, status, headers, config) {
           console.log("There was an error obtaining your saved settings from the database.");
           $scope.settings = api.defaults;
