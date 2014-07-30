55c3ebd331aae35af6637e533453ef01739c14d9
Jeffrey <jeffrey@wix.com>
2014-07-30 09:54:08 -0700

    Saved events are now visible in list view

--- app/client/scripts/controllers/desktop.js
+++ app/client/scripts/controllers/desktop.js
@@ -6,11 +6,11 @@ angular.module('fbCal')
                                        desktopCalendar, list, fbSetup, server) {
 
     //things to prepare for events: order them by time and day & get time/day out
-    var temp1 = {'title' : 'Concert asds duper awesome ereallt can you make it longer yve dfsdf  fsdfsd more things that are teally important baraberque nt come to omg this is the longest event ever my event to have an awesome adventure', 'time' : 'June 17th, 7:30pm', 'day' : 'Wednesday'};
-    var temp2 = {'title' : 'Concert', 'time' : 'June 17th, 9pm', 'day' : 'Wednesday'};
-    var temp3 = {'title' : 'Concert', 'time' : 'June 17th, 8pm', 'day' : 'Wednesday'};
-    var temp4 = {'title' : 'Concertdas Concert asds duper awesome ereallt can you make it longer yve dfsdf  fsdfsd more things that are teally important baraberque nt come to omg this is the lo', 'time' : 'June 17th, 8pm', 'day' : 'Wednesday'};
-    $scope.eventList = [temp1, temp2, temp3, temp4];
+    // var temp1 = {'title' : 'Concert asds duper awesome ereallt can you make it longer yve dfsdf  fsdfsd more things that are teally important baraberque nt come to omg this is the longest event ever my event to have an awesome adventure', 'time' : 'June 17th, 7:30pm', 'day' : 'Wednesday'};
+    // var temp2 = {'title' : 'Concert', 'time' : 'June 17th, 9pm', 'day' : 'Wednesday'};
+    // var temp3 = {'title' : 'Concert', 'time' : 'June 17th, 8pm', 'day' : 'Wednesday'};
+    // var temp4 = {'title' : 'Concertdas Concert asds duper awesome ereallt can you make it longer yve dfsdf  fsdfsd more things that are teally important baraberque nt come to omg this is the lo', 'time' : 'June 17th, 8pm', 'day' : 'Wednesday'};
+    // $scope.eventList = [temp1, temp2, temp3, temp4];
 
     $scope.listStyle = function(last) {
       return list.listStyle(last);
@@ -52,7 +52,7 @@ angular.module('fbCal')
       if ($scope.settings.view === "Month") {
         desktopCalendar.setup(response.fb_event_data);
       } else {
-        list.setup($scope.settings.borderWidth,
+        $scope.eventList = list.setup($scope.settings.borderWidth,
                    response.fb_event_data);
       }
     };
