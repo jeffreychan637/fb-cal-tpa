d2fac85af6bf22745e8476124d08b3e3fda16f26
Jeffrey <jeffrey@wix.com>
2014-07-30 16:53:00 -0700

    Added modal for error and info messages to the user

--- app/client/scripts/controllers/modal.js
+++ app/client/scripts/controllers/modal.js
@@ -5,6 +5,8 @@ angular.module('fbCal')
   .controller('ModalCtrl', function ($scope, $wix, $log, $timeout, eventId, server) {
     $scope.eventId = eventId;
 
+    $('#myModal').modal('show');
+
     if (!$scope.eventId) {
       $timeout(function() {
         $wix.closeWindow('Please open with a valid event');
