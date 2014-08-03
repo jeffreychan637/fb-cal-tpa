23c557cc9fc1a59416cb7ba16ff069960ba1f423
Jeffrey <jeffrey@wix.com>
2014-08-02 18:32:10 -0700

    Event name, location and time are now being displayed in appropriate formats

--- app/client/scripts/controllers/modal.js
+++ app/client/scripts/controllers/modal.js
@@ -5,6 +5,8 @@ angular.module('fbCal')
   .controller('ModalCtrl', function ($scope, $sce, $sanitize, $wix, $log, $timeout, eventId, server) {
     $scope.eventId = eventId;
 
+    var eventInfo;
+
     $scope.eventId = "1512622455616642";
 
     var showErrorModal = function() {
@@ -15,37 +17,60 @@ angular.module('fbCal')
     };
 
     var prepareEventInfo = function() {
+      console.log(eventInfo);
+      console.log(eventInfo.name);
+      $scope.name = eventInfo.name;
+      console.log($scope.name);
       prepareDesciption();
       prepareTime();
+      prepareLocation();
     };
 
     var prepareDesciption = function() {
-      $scope.description = $scope.eventInfo.description.replace(/\r?\n/g, "<br>");
+      $scope.description = eventInfo.description.replace(/\r?\n/g, "<br>");
       $scope.description = $sanitize($scope.description);
       $sce.trustAsHtml($scope.description);
       $scope.showDescription = true;
     };
 
     var prepareTime = function() {
-      var startTime = new Date($scope.eventInfo.start_time);
-      var endTime = new Date($scope.eventInfo.finishTime);
+      var startTime = new Date(eventInfo.start_time);
+      var endTime = new Date(eventInfo.end_time);
       //find a way to tell if they're on the same day
       var onSameDay;
-      var startHour = startTime.toLocaleTimeString().toLocalCase();
-      var endHour = endTime.toLocaleTimeString().toLocalCase();
+      var startHour = startTime.toLocaleTimeString().toLowerCase();
+      var endHour = endTime.toLocaleTimeString().toLowerCase();
       startHour = startHour.replace(/:\d\d /, "");
       endHour = endHour.replace(/:\d\d /, "");
       var startDateString = startTime.toLocaleDateString();
-      var startDayString = startTime.toString().replace(/ .+/, " ,");
+      var startDayString = startTime.toString().replace(/ .+/, ", ");
       if (onSameDay) {
-        $scope.shortTime = startDayString + startDateString + " at " + startHour + "-" + endHour;
+        $scope.shortTime = startDayString + startDateString + " at " +
+                           startHour + "-" + endHour;
       } else {
         var endDateString = endTime.toLocaleDateString();
-        var endDayString = endTime.toString.replace(/ .+/, " ,");
+        var endDayString = endTime.toString().replace(/ .+/, ", ");
         $scope.shortTime = startDayString + startDateString + " - " + endDayString + endDateString;
-        $scope.longTime = 
+        $scope.longTime = startDayString + startDateString + " at " + startHour +
+                          " to " + endDayString + endDateString + " at " +
+                          endHour;
       }
+    };
 
+    var prepareLocation = function() {
+      $scope.location = eventInfo.location;
+      if (eventInfo.venue && (eventInfo.venue.street || eventInfo.venue.city ||
+                              eventInfo.venue.state || 
+                              eventInfo.venue.country)) {
+        $scope.venue = eventInfo.venue.street + ", " + eventInfo.venue.city + 
+                         ", " + eventInfo.venue.state + ", " +
+                         eventInfo.venue.country + " " + eventInfo.venue.zip;
+        $scope.venue = $scope.venue.replace(/, undefined/, "")
+                                       .replace(/undefined, /, "")
+                                       .replace(/undefined/, "")
+                                       .replace(/ $/, "")
+                                       .replace(/, $/, "");
+      }
     };
 
 
@@ -56,22 +81,43 @@ angular.module('fbCal')
         $wix.closeWindow('Closed Modal');
       }, 7000);
     } else {
-      // server.getModalEvent($scope.eventId)
-      //   .then(function(response) {
-      //     $scope.eventInfo = response;
-      //     prepareEventInfo();
-      //   }, function() {
-      //     showErrorModal();
-      //   });
-      $scope.eventInfo = {description: "OMG this is the story of my life. Narwhal bicycle rights aesthetic fanny pack, art party ennui Brooklyn twee typewriter polaroid chia lo-fi Carles drinking vinegar pour-over. Chambray Pinterest ethical banjo church-key whatever hashtag XOXO. American Apparel Banksy twee, paleo bicycle rights readymade bitters. Seitan Wes.\n\nHoodie pork belly DIY pug VHS messenger bag, readymade four loko wolf occupy. Pop-up aesthetic Pitchfork Tumblr. Mumblecore hella DIY, McSweeney's lo-fi meggings yr banh mi trust fund Odd Future cardigan disrupt sartorial kale chips. Pitchfork organic keytar, roof party street art PBR&amp;B Tumblr church-key High Life beard +1.", 
-                          name: 'My Event (Demo for Wix App) I want to see a super duper long title what will it do', 
-                          start_time: '2014-07-11T19:00:00-0700', 
-                          location: '', 
-                          timezone: 'America/Los_Angeles', 
-                          id: '1512622455616642', 
-                          end_time: '2014-07-11T22:00:00-0700', 
-                          eventColor: '#234323'};
-      prepareEventInfo();
+      server.getModalEvent($scope.eventId)
+        .then(function(response) {
+          //get cover photo here
+          //  then cover photo
+          //    then guest links
+          //      feed
+          eventInfo = response;
+          prepareEventInfo();
+        }, function() {
+          showErrorModal();
+        });
+      // eventInfo = {
+      //               "description": "OMG this is the story of my life. Narwhal bicycle rights aesthetic fanny pack, art party ennui Brooklyn twee typewriter polaroid chia lo-fi Carles drinking vinegar pour-over. Chambray Pinterest ethical banjo church-key whatever hashtag XOXO. American Apparel Banksy twee, paleo bicycle rights readymade bitters. Seitan Wes.\n\nHoodie pork belly DIY pug VHS messenger bag, readymade four loko wolf occupy. Pop-up aesthetic Pitchfork Tumblr. Mumblecore hella DIY, McSweeney's lo-fi meggings yr banh mi trust fund Odd Future cardigan disrupt sartorial kale chips. Pitchfork organic keytar, roof party street art PBR&B Tumblr church-key High Life beard +1.", 
+      //               "end_time": "2014-08-09T22:00:00-0700", 
+      //               "is_date_only": false, 
+      //               "location": "The Shark Tank - Sap Center", 
+      //               "name": "My Event (Demo for Wix App)", 
+      //               "owner": {
+      //                 "id": "671074532971418", 
+      //                 "name": "Jeffrey Chan"
+      //               }, 
+      //               "privacy": "SECRET", 
+      //               "start_time": "2014-08-08T19:00:00-0700", 
+      //               "timezone": "America/Los_Angeles", 
+      //               "updated_time": "2014-08-01T23:38:37+0000", 
+      //               "venue": {
+      //                 "id": "411435258956162", 
+      //                 "city": "San Jose", 
+      //                 "country": "United States", 
+      //                 "latitude": 37.332655187978, 
+      //                 "longitude": -121.90106628747, 
+      //                 "state": "CA", 
+      //                 "zip": "95113"
+      //               }, 
+      //               "id": "1512622455616642"
+      //             };
+      // prepareEventInfo();
     }
 
 });
